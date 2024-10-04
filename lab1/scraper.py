c33ada34e7279d9d03ebb452b7d92c25fae5a371
YourBestDream <ferari100w3@gmail.com>
2024-10-04 15:04:32 +0300

    Extract additional data; add 2 validations for data scraped

--- lab1/scraper.py
+++ lab1/scraper.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag, ResultSet
 
 url = "https://darwin.md/gadgets"
 
@@ -11,10 +11,35 @@ try:
     #     file.write(response.text)
     soup = BeautifulSoup(response.text, "html.parser")
     
-    items = soup.find("div", class_ = "item-products")
+    item_products = soup.find("div", class_ = "item-products")
+    if item_products is None or not isinstance(item_products, Tag):
+        raise ValueError("Required div with class 'item-products' hasn't been found")
+    
+    items = item_products.find_all("figure")
+    if not items or not isinstance(items, ResultSet):
+        raise ValueError("Required figure tags hasn't been found within div")
+    
     for item in items:
-        print("============")
-        print(item)
+        name_tag = item.find("a", class_ = "d-block")
+        if name_tag is None or not isinstance(name_tag, Tag):
+            continue
+        else:
+            name = name_tag.text.strip()
+        
+        price_tag = item.find("span", class_ = "price-new")
+        if price_tag is None or not isinstance(price_tag, Tag):
+            continue
+        else:
+            price = price_tag.text.strip()
+        
+        specifications_tag = item.find("span", class_ = "specification")
+        if specifications_tag:
+            continue
+        else:
+            specifications = specifications_tag.text.strip().replace(",","|").split("|")
+
+        print(f"Name: {name},\nPrice: {price},\nSpecifications: {specifications}\n")
+
 except requests.exceptions.HTTPError as exc:
     print(f"Something went wrong: {exc}")
     
